f15e09656a3b15168f29e9f9fe0247dd42bf410b
Michael Potter <mgp140@gmail.com>
2023-07-01 09:52:38 -0400

    feat: added version to cli

--- bin/cli.js
+++ bin/cli.js
@@ -2,13 +2,21 @@
 
 import fs from 'node:fs';
 import fsPromises from 'node:fs/promises';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { Command } from 'commander';
 import { browser } from '../src/browser.js';
+
+// Get pkg info
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const pkg = JSON.parse(fs.readFileSync(join(__dirname, '../package.json'), 'utf8'));
 
 const program = new Command();
 program
   .argument('<file>', 'file to execute in the browser.')
   .option('--url [string]', 'specify a specific url to execute the code in.')
+  .version(pkg.version)
   // .option('--headless [boolean]', 'specify running the browser in headless mode.')
   // .option('--watch [boolean]', 'rerun the file on change.');
 program.parse(process.argv);
