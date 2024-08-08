a88b476cba50e3ed64d3a39291f21649d89b8b4e
Michael Potter <mgp140@gmail.com>
2024-08-07 21:22:02 -0400

    feat: support ts config files

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 import { join } from 'node:path';
-import { stat } from 'node:fs/promises';
 import type { Page, BrowserContext, Browser } from 'playwright';
+import { build } from 'esbuild';
+import { exists, esm } from './utils.js';
 
 export type PlaywrightConfig = {
   page: Page,
@@ -16,26 +17,59 @@ export type Config = {
   playwright?: (page: PlaywrightConfig) => Promise<void>
 }
 
-const exists = (path:string) => stat(path).then(() => true, () => false);
+/**
+ * Looks at the users root path to see if there
+ * are any valid config files.
+ * @returns either the absolute path to the valid config
+ *          or returns false if none have config
+ *          has not been found.
+ */
+async function getConfigPath(): Promise<string | false> {
+  if (await exists(join(process.cwd(), 'scratchpad.config.js'))) {
+    return join(process.cwd(), 'scratchpad.config.js');
+  }
+  if (await exists(join(process.cwd(), 'scratchpad.config.ts'))) {
+    return join(process.cwd(), 'scratchpad.config.ts');
+  }
+  return false;
+}
 
-async function importConfig(rootDir:string) {
-  const path = join(rootDir, './scratchpad.config.js');
-  if (await exists(path)) {
-    return import(path)
-      .then(x => x.default)
-      .catch(e => {
-        console.error(e);
-        return {};
-      });
-  } else {
-    return {};
+/**
+ * Get the import config object from the config file.
+ */
+async function importConfig(): Promise<Partial<Config>> {
+  // determine if there are any valid configs
+  const configPath = await getConfigPath();
+
+  // if there is no config path then return
+  // an empty object
+  if (!configPath) {
+    return {}
+  }
+
+  try {
+    // use esbuild to load the .js or .ts file
+    const { outputFiles: [stdout] } = await build({
+      entryPoints: [configPath],
+      format: 'esm',
+      bundle: true,
+      write: false,
+    });
+    const contents = new TextDecoder().decode(stdout.contents);
+    const module = await import(esm`${contents}`);
+    return module.default ?? {};
+  } catch (e) {
+    console.error(`An error occured parsing config file.${configPath}`);
+    console.error(``);
+    console.error(`Path: ${configPath}`);
+    console.error(``);
+    throw e;
   }
 }
 
 export async function getConfig(): Promise<Config> {
-  const rootDir = process.cwd();
   return {
-    ...await importConfig(rootDir),
+    ...await importConfig(),
   }
 }
 
